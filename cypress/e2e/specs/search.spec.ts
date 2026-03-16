a6dbf542f4421400f1e5aff100d6b68423461056
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2026-03-16 09:16:11 +0200

    Update search test to use 'winter' track

--- cypress/e2e/specs/search.spec.ts
+++ cypress/e2e/specs/search.spec.ts
@@ -7,7 +7,7 @@ describe('Test Case 1,2,3', () => {
   })
 
   it('Should test search functionality', () => {
-    PlaylistPage.fillSearchInput(Cypress.env('track_name'))
+    PlaylistPage.fillSearchInput('winter')
     PlaylistPage.winterTrack.should('be.visible');
     PlaylistPage.summerTrack.should('not.exist');
     PlaylistPage.autumnTrack.should('not.exist');
